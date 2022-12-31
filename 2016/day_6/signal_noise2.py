67744feb2c9394ed01eed7cd7e43051f09a51b65
tsioros <ioannis.tsioros@nokia.com>
2022-12-31 17:20:53 +0200

    2016_day6_part2

---
+++ 2016/day_6/signal_noise2.py
@@ -0,0 +1,34 @@
+import re
+from collections import Counter
+
+
+def parse_input(file1):
+    input_ls = []
+    with open(file1) as reader:
+        flines = reader.readlines()
+        for item in flines:
+            input_ls.append(item.rstrip())
+    return input_ls
+
+
+if __name__ == "__main__":
+    file = 'input.txt'
+    # file = 'example.txt'
+    lines = parse_input(file)
+    print(lines)
+    chars_d = {}
+    for line in lines:
+        for count, char in enumerate(line):
+            chars_d[count] = chars_d.get(count, '') + char
+
+    print(chars_d)
+
+    signal = ''
+    signal_least_common = ''
+    for k, v in chars_d.items():
+        most_common_char = Counter(v).most_common(1)[0][0]
+        least_common_char = Counter(v).most_common()[-1][0]
+        print(f"{k = }, {v = } // {most_common_char = } // {least_common_char = }")
+        signal += most_common_char
+        signal_least_common += least_common_char
+    print(f"{signal = } // {signal_least_common = }")
